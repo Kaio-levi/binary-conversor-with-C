2e3abcdf97cf1f369185327df928ba196cb64748
Kaio Levi <kaiolevipollhein@gmail.com>
2026-02-27 10:51:37 -0300

    nova solução

--- converter_decimal_em_binario.c
+++ converter_decimal_em_binario.c
@@ -3,14 +3,23 @@
 int main(){
     int numero_Decimal;
     int numero_Binairo;
+    int vet_binario[8];
+    int contador = 0;
 
     printf("digite um numero inteiro: \n");
     scanf("%d",&numero_Decimal);
 
-    while(numero_Decimal > 0){
+
+    for(int i = 7; i >= 0 ; i--){
         numero_Binairo = numero_Decimal % 2;
-        printf("%d", numero_Binairo);
+        vet_binario[i] = numero_Binairo;
         numero_Decimal = numero_Decimal / 2;
     }
+
+    printf("O numero em binario é: ");
+    for(int i = 0; i < contador; i++){
+        printf("%d", vet_binario[i]);
+    }
+
     return 0;
 }
